9cca07812eb3d41c7608136e2238c5774abd4b61
joeflack4 <joeflack4@gmail.com>
2016-10-20 09:59:55 -0400

    Hashing dependency call changed for differences in local environment.

--- application/models.py
+++ application/models.py
@@ -1,5 +1,14 @@
 from index import db
 from werkzeug.security import generate_password_hash, check_password_hash
+# Might want to change code to be like the following.
+# def self._password = User.hashed_password(password)
+# @property
+# def password(self):
+#     return self._password
+#
+# @password.setter
+# def set_password(self, password):
+#     self._password = generate_password_hash(password)
 
 
 class User(db.Model):
@@ -14,12 +23,24 @@ class User(db.Model):
 
     @staticmethod
     def hashed_password(password):
-        return generate_password_hash(password)
+        # Hacky Solution ;(. For some reason, the former is working on Heroku, but the latter is working locally. Maybe reset my virtual environment and try again?
+        try:
+            return generate_password_hash(password)
+        except:
+            from index import bcrypt
+            return bcrypt.generate_password_hash(password)
 
     @staticmethod
     def get_user_with_email_and_password(email, password):
         user = User.query.filter_by(email=email).first()
-        if user and check_password_hash(user.password, password):
+        # See above: 'Hacky Solution'.
+        try:
+            pw_ok = check_password_hash(user.password, password)
+        except:
+            from index import bcrypt
+            pw_ok = bcrypt.check_password_hash(user.password, password)
+
+        if user and pw_ok:
             return user
         else:
             return None
