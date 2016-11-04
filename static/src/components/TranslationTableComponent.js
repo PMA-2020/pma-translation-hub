b09843c018f63a3732cde43171f8fa4b9148b8a6
joeflack4 <joeflack4@gmail.com>
2016-11-04 12:31:53 -0400

    Added dynamic language field generation for tables.

--- static/src/components/TranslationTableComponent.js
+++ static/src/components/TranslationTableComponent.js
@@ -32,25 +32,49 @@ function mapDispatchToProps(dispatch) {
 *  Data Conversion Process
 *  The following logic converts data as presented by database into a format for display in a table.
 * */
-// # Iterate through each entry in data by ID.
 var translationTexts = []
-Object.entries(data).forEach(([key]) => {
-    var item = {}
-    item.id = key
 
-    // # Iterate through each key/val of the entry.
-    Object.entries(data[key]).forEach(([key2, value]) => {
-        if (typeof value !== 'object') {
-            item[key2] = value
-        } else if (typeof value === 'object') {
+    // var rawData = rawDataList
+    // var filteredLanguages = []
+    //
+    // // # Iterate through each entry.
+    // Object.entries(rawData).forEach(([entryKey]) => {
+    //     // var language
+    //
+    //     // # Iterate through each language of each entry.
+    //     Object.entries(data[entryKey].languages).forEach(([languageKey]) => {
+    //         if ((languageKey in filteredLanguages) !== true) {
+    //             filteredLanguages.push(languageKey)
+    //         }
+    //     })
+    // })
+    //
+    // filteredLanguages = ["english", "swahili"]
+    // return filteredLanguages
+
+
+
+
+
+// # # The following code converts dataset from an object to a list. This conversion helps the data be read by the table.
+// # Iterate through each entry in data by ID.
+Object.entries(data).forEach(([entryKey]) => {
+    var item = {}
+    item.id = entryKey
+
+    // # Iterate through each key/val property of the entry.
+    Object.entries(data[entryKey]).forEach(([propertyKey, propertyValue]) => {
+        if (typeof propertyValue !== 'object') {
+            item[propertyKey] = propertyValue
+        } else if (typeof propertyValue === 'object') {
 
             // # Iterate through each key/val of the languages property of the entry.
-            Object.entries(data[key][key2]).forEach(([key3, value2]) => {
-                item[key3] = value2
+            Object.entries(data[entryKey][propertyKey]).forEach(([languageKey, languageValue]) => {
+                item[languageKey] = languageValue
             })
 
         } else {
-            item[key2] = 'Error reading item value.'
+            item[propertyKey] = 'Error reading item value.'
         }
     })
     translationTexts.push(item)
@@ -93,7 +117,6 @@ Object.entries(data).forEach(([key]) => {
 //         console.log(row.id);
 //     }
 // }
-
 
 // @connect(mapStateToProps, mapDispatchToProps)
 @connect(mapStateToProps, mapDispatchToProps)
@@ -163,47 +186,61 @@ export default class TranslationTable extends React.Component {
         return filteredData
     }
 
+    fetchLanguages() {
+        var filteredLanguages = []
+
+        // # Iterate through each entry.
+        Object.entries(data).forEach(([entryKey]) => {
+            // var language
+
+            // # Iterate through each language of each entry.
+            Object.entries(data[entryKey].languages).forEach(([languageKey]) => {
+                if (!filteredLanguages.includes(languageKey) && (data[entryKey].status === this.props.dataType)) {
+                    filteredLanguages.push(languageKey)
+                }
+                // if (!languages.includes(language)) {
+                //     languages.push(language)
+                // }
+            })
+        })
+
+        return filteredLanguages
+    }
+
     data = this.fetchData()
 
-    // * It's a data format example.
-    // Not working for some reason.
-    // priceFormatter(cell, row) {
-    //   return '<i class="glyphicon glyphicon-usd"></i> ' + cell
-    // }
+    languages = this.fetchLanguages()
 
     render() {
+        const fields = [['id', '#'], ['type', 'Type'], ['questionAppearances', 'Appearances']]
+        // # Push language identifier and label to the list of fields to be rendered.
+        for (let language of this.languages) {
+            fields.push([language, _.capitalize(language)])
+        }
+
         return (
             <div>
                 {/*TODO: Remove this test below. */}
                 {/*<h1 onClick={(e) => this.openDrawer(e)}>{this.props.title}</h1>*/}
                 {/*<h1 onClick={() => this.openDrawer()}>{this.props.title}</h1>*/}
 
-                {/*<strong>Testing Ground</strong>*/}
-                {/*<br/>*/}
-                {/*ID: {translationTextsNew[3].id}*/}
-                {/*<br/>*/}
-                {/*Type: {translationTextsNew[3].type}*/}
-                {/*<br/>*/}
-                {/*English: {translationTextsNew[3].french}*/}
-                {/*<hr/>*/}
-
-                <h1>{this.props.title}</h1>
+                {/* # Other options for tables. */}
                 {/*<BootstrapTable data={translationTexts} selectRow={selectRowProp} options={options} striped={true} hover={true} pagination={true} columnFilter={true} search={true} clearSearch={true} insertRow={true} exportCSV={true} bordered={false} height="100%" maxHeight={300}>*/}
                 {/*<BootstrapTable data={translationTexts} selectRow={selectRowProp} striped={true} hover={true} pagination={true} columnFilter={true} search={true} clearSearch={true} insertRow={true} exportCSV={true} bordered={false} height="100%" maxHeight={300}>*/}
-                <BootstrapTable data={this.data} options={this.options} striped={true} hover={true} pagination={true} columnFilter={true} search={true} clearSearch={true} insertRow={true} exportCSV={true} bordered={false} height="100%" maxHeight={300}>
                 {/*<BootstrapTable data={translationTexts} striped={true} hover={true} pagination={true} columnFilter={true} search={true} clearSearch={true} insertRow={true} exportCSV={true} bordered={false} height="100%" maxHeight={300}>*/}
                 {/*<BootstrapTable data={translationTexts}>*/}
-                    <TableHeaderColumn dataField="id" isKey={true} dataAlign="center" dataSort={true}>#</TableHeaderColumn>
-                    <TableHeaderColumn dataField="type" dataSort={true}>Type</TableHeaderColumn>
-                    <TableHeaderColumn dataField="english" dataSort={true}>English</TableHeaderColumn>
-                    <TableHeaderColumn dataField="swahili" dataSort={true}>Swahili</TableHeaderColumn>
-                    <TableHeaderColumn dataField="french" dataSort={true}>French</TableHeaderColumn>
-                    <TableHeaderColumn dataField="questionAppearances" dataSort={false}>In Questions</TableHeaderColumn>
-                    {/*Not working for some reason.*/}
-                    {/*<TableHeaderColumn dataField="price" dataFormat={priceFormatter}>Product Price</TableHeaderColumn>*/}
+
+                <h1>{this.props.title}</h1>
+                <BootstrapTable data={this.data} keyField={fields[0][0]} options={this.options} striped={true} hover={true} pagination={true} search={true}
+                                clearSearch={true} insertRow={true} exportCSV={true} bordered={false}>
+                                {/* # Other option for table properties. */}
+                                {/*{height="100%"}*/}
+                                {/*maxheight={300}*/}
+                                {/*columnFilter={true}*/}
+
+                    { fields.map(([field, label]) => <TableHeaderColumn dataField={ field }>{ label }</TableHeaderColumn>) }
+
                 </BootstrapTable>
-                <br/>
-                <br/>
             </div>
         )
     }
